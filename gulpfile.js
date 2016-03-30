3d518b97f377b3de5f8cde5a6671088490b10e13
Bilal Kazi <bilalahmedkazi@hotmail.com>
2016-03-31 00:02:57 +0100

    Add tasks for running the jekyll build and compile sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var bs = require('browser-sync');
+var bs = require('browser-sync').create();
 var sass = require('gulp-sass');
 var prefixer = require('gulp-autoprefixer');
 var cp = require('child_process');
@@ -21,4 +21,42 @@ var jekyll = process.platform == 'win32' ? 'jekyll.bat' : 'jekyll';
 /**
 * Command line options for jekyll if needed
 */
-var jekyllOptions = '';
+var jekyll_options = '';
+
+/**
+* Spawns a process to do the Jekyll build
+*/
+gulp.task('buildJekyll', done => {
+	var jekyll_process = cp.spawn( jekyll, ['build', jekyll_options], {stdio: 'inherit'} )
+	jekyll_process.on('close', done);
+	return jekyll_process;
+});
+
+/**
+ * Compile Sass files
+ */
+gulp.task('sass', () => {
+    return gulp.src('_sass/main.scss')
+        .pipe(sass({
+            onError: bs.notify
+        }))
+        .pipe(prefixer(browsers))
+        .pipe(gulp.dest('css/'))
+        .pipe(bs.stream());
+});
+
+/**
+* Default task
+* Makes calls to other tasks
+*/
+gulp.task(
+    'default',
+    gulp.series(
+        gulp.series('sass', 'buildJekyll'),
+        () => {
+            bs.init({
+                server: './_site'
+            })
+        }
+    )
+);
