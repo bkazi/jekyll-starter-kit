27e2392675d3bd606710164aee3d3acea042c360
Bilal Kazi <bilalahmedkazi@hotmail.com>
2016-03-31 22:34:11 +0100

    Add separate build task for best performance

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,63 @@
-'use strict'
+'use strict';
 var gulp = require('gulp');
 var bs = require('browser-sync');
 var sass = require('gulp-sass');
 var prefixer = require('gulp-autoprefixer');
 var cp = require('child_process');
+var imagemin = require('gulp-imagemin');
 
 /**
 * Object containing paths to all useful directories
 */
-var paths = {
+const paths = {
     styles: {
         src: ['_sass/*.scss'],
         dest: '_includes/'
     },
     contentFiles: {
         src: ['*.html', '_layouts/*,html', '_includes/*.html', '_posts/*']
+    },
+    img: {
+        src: ['img/*'],
+        dest: 'img/'
     }
 };
-var msgJekyll = "Running: $ jekyll build"
+const msgJekyll = "Running: $ jekyll build"
 
 /**
 * Array defining browser versions to auto prefix for
 * Refer to https://github.com/ai/browserslist#queries
 * for all possibilities
 */
-var browsers = ['last 3 versions'];
+const browsers = ['last 3 versions'];
 
 /**
 * The jekyll command to use to spawn the child process
 * Requires check for Windows since node has a quirk in
 * how it handles child processes
 */
-var jekyll = process.platform == 'win32' ? 'jekyll.bat' : 'jekyll';
+const jekyll = process.platform == 'win32' ? 'jekyll.bat' : 'jekyll';
 
 /**
 * Command line options for jekyll if needed
+* Array containing strings, build is compulsary
 */
-var jekyll_options = '';
+const jekyll_options = ['build'];
+
+/**
+* An object containing options for image optimisation
+* https://github.com/sindresorhus/gulp-imagemin#imageminoptions
+*/
+const imageMinOptions = {
+    progressive: true,
+    interlaced: true
+};
 
 /**
 * Spawns a process to do the Jekyll build
 */
 gulp.task('buildJekyll', done => {
-	var jekyll_process = cp.spawn( jekyll, ['build', jekyll_options], {stdio: 'inherit'} )
+	var jekyll_process = cp.spawn( jekyll, jekyll_options, {stdio: 'inherit'} )
 	jekyll_process.on('close', done);
     bs.notify(msgJekyll);
 	return jekyll_process;
@@ -66,6 +81,29 @@ gulp.task('sass', () => {
         .pipe(prefixer(browsers))
         .pipe(gulp.dest(paths.styles.dest))
         .pipe(bs.stream());
+});
+
+/**
+* Add a final build version of sass task
+*/
+gulp.task('sass-build', () => {
+    return gulp.src(paths.styles.src)
+        .pipe(sass({
+            onError: bs.notify,
+            outputStyle: 'compressed'
+        }))
+        .pipe(prefixer(browsers))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(bs.stream());
+});
+
+/**
+* Optimize images
+*/
+gulp.task('img-optimize', () => {
+    return gulp.src(paths.img.src)
+        .pipe(imagemin(imageMinOptions))
+        .pipe(gulp.dest(paths.img.dest));
 });
 
 /**
@@ -103,5 +141,19 @@ gulp.task(
         'sass',
         'buildJekyll',
         gulp.parallel('browserSync', 'watch')
+    )
+);
+
+/**
+* Build task
+*/
+gulp.task(
+    'build',
+    gulp.series(
+        gulp.parallel(
+            'sass-build',
+            'img-optimize'
+        ),
+        'buildJekyll'
     )
 );
